5c770727ae28a71fa81f058c7b9c53d4dcbff586
William Trevisan <williamtrev@hotmail.com>
2022-05-30 12:03:53 -0300

    feat: create MagicMethodsTrait with a __get magic method

---
+++ src/Core/Domain/Entity/Traits/MagicMethodsTrait.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Core\Domain\Entity\Traits;
+
+use Exception;
+
+trait MagicMethodsTrait
+{
+    public function __get($property)
+    {
+        if (! isset($this->{$property})) {
+            $className = get_class($this);
+
+            throw new Exception("Property: $property not found in class $className");
+        }
+
+        return $this->{$property};
+    }
+}
