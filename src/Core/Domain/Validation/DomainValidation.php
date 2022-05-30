eebc51151adba4b021dd2ba8c35a6450d41bb845
William Trevisan <williamtrev@hotmail.com>
2022-05-30 14:31:52 -0300

    feat(domain-validation): created a strMaxLength method in a DomainValidation.

--- src/Core/Domain/Validation/DomainValidation.php
+++ src/Core/Domain/Validation/DomainValidation.php
@@ -9,7 +9,21 @@ class DomainValidation
     public static function notNull(string $value, string $exceptionMessage = null)
     {
         if (! $value) {
-            throw new EntityValidationException($exceptionMessage ?? 'Should not empty or null');
+            throw new EntityValidationException(
+                $exceptionMessage ?? 'Should not be empty or null'
+            );
+        }
+    }
+
+    public static function strMaxLength(
+        string $value,
+        int $length = 255,
+        string $exceptionMessage = null
+    ) {
+        if (strlen($value) <= $length) {
+            throw new EntityValidationException(
+                $exceptionMessage ?? "The value must not be greater than $length characters"
+            );
         }
     }
 }
