c9dd85a13d3915e7020d1f27b68a69cd12bd64bb
William Trevisan <williamtrev@hotmail.com>
2022-05-30 17:17:19 -0300

    feat(category): created a initial structure for a createCategoryUseCase

--- src/Core/UseCase/Category/CreateCategoryUseCase.php
+++ src/Core/UseCase/Category/CreateCategoryUseCase.php
@@ -2,7 +2,21 @@
 
 namespace Core\UseCase\Category;
 
+use Core\Domain\Entity\Category;
+use Core\Domain\Repository\CategoryRepositoryInterface;
+
 class CreateCategoryUseCase
 {
+    public function __construct(
+        protected readonly CategoryRepositoryInterface $categoryRepository
+    ) {}
 
+    public function execute()
+    {
+        $category = new Category(
+            name: 'Category name'
+        );
+
+        $this->categoryRepository->insert($category);
+    }
 }
