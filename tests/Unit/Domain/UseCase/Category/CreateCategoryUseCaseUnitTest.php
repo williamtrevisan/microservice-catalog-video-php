5dab0c78ecb10a8c5f2025a2948740ab32988b7f
William Trevisan <williamtrev@hotmail.com>
2022-05-30 17:06:53 -0300

    feat(category): created initial testCreateNewCategory into a CreateCategoryUseCaseUnitTest

--- tests/Unit/Domain/UseCase/Category/CreateCategoryUseCaseUnitTest.php
+++ tests/Unit/Domain/UseCase/Category/CreateCategoryUseCaseUnitTest.php
@@ -2,9 +2,25 @@
 
 namespace Tests\Unit\Domain\UseCase\Category;
 
+use Core\Domain\Entity\Category;
+use Core\Domain\Repository\CategoryRepositoryInterface;
+use Core\UseCase\Category\CreateCategoryUseCase;
+use Mockery;
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
 class CreateCategoryUseCaseUnitTest extends TestCase
 {
+    public function testCreateNewCategory()
+    {
+        $categoryId = '1';
+        $categoryName = 'Category name';
+        $categoryEntity = Mockery::mock(Category::class, [$categoryId, $categoryName]);
+        $categoryRepository =
+            Mockery::mock(stdClass::class, CategoryRepositoryInterface::class);
+        $categoryRepository->shouldReceive('insert')->andReturn($categoryEntity);
 
+        $createCategoryUseCase = new CreateCategoryUseCase($categoryRepository);
+        $createCategoryUseCase->execute();
+    }
 }
