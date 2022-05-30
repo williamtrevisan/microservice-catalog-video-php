001776f25d7e9cf7019fe7734f8810b4c71c87b9
William Trevisan <williamtrev@hotmail.com>
2022-05-30 17:45:13 -0300

    test(category): Finished testCreateNewCategory in a CreateCategoryUseCaseUnitTest.

---
+++ tests/Unit/UseCase/Category/CreateCategoryUseCaseUnitTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit\UseCase\Category;
+
+use Core\Domain\Entity\Category;
+use Core\Domain\Repository\CategoryRepositoryInterface;
+use Core\UseCase\Category\CreateCategoryUseCase;
+use Core\UseCase\DTO\Category\CreateCategoryInputDTO;
+use Core\UseCase\DTO\Category\CreateCategoryOutputDTO;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+use stdClass;
+
+class CreateCategoryUseCaseUnitTest extends TestCase
+{
+    public function testCreateNewCategory()
+    {
+        $categoryId = Uuid::uuid4()->toString();
+        $categoryName = 'Category name';
+        $categoryEntity = Mockery::mock(Category::class, [
+            $categoryId,
+            $categoryName,
+        ]);
+        $categoryEntity->shouldReceive('id')->andReturn($categoryId);
+        $categoryRepository =
+            Mockery::mock(stdClass::class, CategoryRepositoryInterface::class);
+        $categoryRepository->shouldReceive('insert')->andReturn($categoryEntity);
+        $createCategoryInputDTO = Mockery::mock(CreateCategoryInputDTO::class, [
+            $categoryName,
+        ]);
+
+        $createCategoryUseCase = new CreateCategoryUseCase($categoryRepository);
+        $response = $createCategoryUseCase->execute($createCategoryInputDTO);
+
+        $this->assertInstanceOf(CreateCategoryOutputDTO::class, $response);
+
+        Mockery::close();
+    }
+}
