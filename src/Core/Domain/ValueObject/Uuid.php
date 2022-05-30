a05a6d0915d79e395509a3f37c3a007adc0f28a1
William Trevisan <williamtrev@hotmail.com>
2022-05-30 16:03:48 -0300

    feat: added ramseyUuid and created uuid value object.

---
+++ src/Core/Domain/ValueObject/Uuid.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Core\Domain\ValueObject;
+
+use InvalidArgumentException;
+use Ramsey\Uuid\Nonstandard\Uuid as RamseyUuid;
+
+class Uuid
+{
+    public function __construct(
+        protected string $value
+    ) {
+        $this->ensureIsValid($value);
+    }
+
+    public static function random(): self
+    {
+        return new self(RamseyUuid::uuid4()->toString());
+    }
+
+    public function __toString(): string
+    {
+        return $this->value;
+    }
+
+    private function ensureIsValid(string $value)
+    {
+        $isValid = RamseyUuid::isValid($value);
+        if (! $isValid) {
+            throw new InvalidArgumentException(
+                sprintf('<%s> does not allow the value <%s>.', static::class, $value)
+            );
+        }
+    }
+}
