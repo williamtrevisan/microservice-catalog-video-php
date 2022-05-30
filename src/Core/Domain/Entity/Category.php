7a1f8e146969ec34d1b0e1d914621e46809f24b5
William Trevisan <williamtrev@hotmail.com>
2022-05-30 16:33:31 -0300

    feat(category): included createdAt into a category entity contruct

--- src/Core/Domain/Entity/Category.php
+++ src/Core/Domain/Entity/Category.php
@@ -5,6 +5,7 @@ namespace Core\Domain\Entity;
 use Core\Domain\Entity\Traits\MagicMethodsTrait;
 use Core\Domain\Validation\DomainValidation;
 use Core\Domain\ValueObject\Uuid;
+use DateTime;
 
 class Category
 {
@@ -14,9 +15,14 @@ class Category
         protected Uuid|string $id = '',
         protected string $name = '',
         protected string $description = '',
-        protected bool $isActive = true
+        protected bool $isActive = true,
+        protected DateTime|string $createdAt = '',
     ) {
         $this->id = $this->id ? new Uuid($this->id) : Uuid::random();
+        $this->createdAt =
+            $this->createdAt
+                ? new DateTime($this->createdAt)
+                : new DateTime();
 
         $this->validate();
     }
