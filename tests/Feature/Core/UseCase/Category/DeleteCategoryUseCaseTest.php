fb45f64d0c50f7996f1de0f45c0c390249a0fcb3
William Trevisan <williamtrev@hotmail.com>
2022-06-12 12:24:39 -0300

    test(category): created a integration test for delete use case

---
+++ tests/Feature/Core/UseCase/Category/DeleteCategoryUseCaseTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Category;
+
+use App\Models\Category as CategoryModel;
+use App\Repositories\Eloquent\CategoryEloquentRepository;
+use Core\UseCase\Category\DeleteCategoryUseCase;
+use Core\UseCase\DTO\Category\CategoryInputDTO;
+use Core\UseCase\DTO\Category\delete\DeleteCategoryOutputDTO;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DeleteCategoryUseCaseTest extends TestCase
+{
+    public function testDelete()
+    {
+        $categoryDatabase = CategoryModel::factory()->create();
+        $categoryRepository = new CategoryEloquentRepository(new CategoryModel());
+        $categoryInputDTO = new CategoryInputDTO(id: $categoryDatabase->id);
+
+        $deleteCategoryUseCase = new DeleteCategoryUseCase($categoryRepository);
+        $response = $deleteCategoryUseCase->execute($categoryInputDTO);
+
+        $this->assertInstanceOf(DeleteCategoryOutputDTO::class, $response);
+        $this->assertSoftDeleted($categoryDatabase);
+    }
+}
