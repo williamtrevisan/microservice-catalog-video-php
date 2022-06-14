f3b34b7ad4a1f8d14d8b79c6da6de711ca5c9bf3
William Trevisan <williamtrev@hotmail.com>
2022-06-14 14:02:26 -0300

    test(genre): created all tests for GenreUnitTest model

---
+++ tests/Unit/App/Models/GenreUnitTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit\App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class GenreUnitTest
+{
+    protected function getModel(): Model
+    {
+        return new Genre();
+    }
+
+    protected function getTraits(): array
+    {
+        return [HasFactory::class, SoftDeletes::class];
+    }
+
+    protected function getFillables(): array
+    {
+        return [
+            'id',
+            'name',
+            'is_active',
+        ];
+    }
+
+    protected function getCasts(): array
+    {
+        return [
+            'id' => 'string',
+            'is_active' => 'boolean',
+            'deleted_at' => 'datetime',
+        ];
+    }
+}
